752eb2f5190c7b626f0cf184215214fc428df8fd
ZetaZiegler <zieglercristian14@gmail.com>
2024-07-01 21:55:24 -0300

    Update script.js
    Agregamos requerimientos de unidad y lenguaje al fetch. Mostramos la velocidad del viento en el DOM.

--- Aplicacion del clima/script.js
+++ Aplicacion del clima/script.js
@@ -1,7 +1,5 @@
 let urlBase = 'https://api.openweathermap.org/data/2.5/weather'
 let api_key = '923104f3766160f6bd277a03e3e56129'
-let difKelvin = 273.15
-
 
 document.getElementById('botonBusqueda').addEventListener('click', () => {
     const ciudad = document.getElementById('ciudadEntrada').value
@@ -11,9 +9,10 @@ document.getElementById('botonBusqueda').addEventListener('click', () => {
 })
 
 function fetchDatosClima(ciudad){
-    fetch(`${urlBase}?q=${ciudad}&appid=${api_key}`)
+    fetch(`${urlBase}?q=${ciudad}&appid=${api_key}&units=metric&lang=es`)
     .then(data => data.json())
     .then(data => mostrarDatosClima(data))
+    console.log(data)
 }
 
 function mostrarDatosClima(data){
@@ -26,12 +25,14 @@ function mostrarDatosClima(data){
     const pais = data.sys.country
     const icono = data.weather[0].icon
     const humedad = data.main.humidity
+    const veloViento = data.wind.speed
+    const direViento = data.wind.deg
 
     const ciudadTitulo = document.createElement('h2')
     ciudadTitulo.textContent = `${ciudadNombre}, ${pais}`
 
     const temperaturaInfo = document.createElement('p')
-    temperaturaInfo.textContent= `La temperatura es: ${Math.floor(temperatura-difKelvin)}°C`
+    temperaturaInfo.textContent= `La temperatura es: ${Math.floor(temperatura)}°C`
 
     const humedadInfo = document.createElement('p')
     humedadInfo.textContent = `La humedad es: ${humedad}%`
@@ -40,11 +41,15 @@ function mostrarDatosClima(data){
     iconoInfo.src= `https://openweathermap.org/img/wn/${icono}@2x.png`
 
     const descripcionInfo = document.createElement('p')
-    descripcionInfo.textContent = `La descripcion meteorológica es: ${descripcion}`
+    descripcionInfo.textContent = `El pronóstico es ${descripcion}`
+
+    const vientoInfo = document.createElement('p')
+    vientoInfo.textContent = `La velocidad del viento es de ${Math.floor(veloViento * 3.6)} Km/h`
 
     divDatosClima.appendChild(ciudadTitulo)
     divDatosClima.appendChild(temperaturaInfo)
     divDatosClima.appendChild(humedadInfo)
+    divDatosClima.appendChild(vientoInfo)
     divDatosClima.appendChild(iconoInfo)
     divDatosClima.appendChild(descripcionInfo)
 }
